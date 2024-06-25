fe7c4f09fd76c581711b21f09b80869987ae1cf6
kirankumarkommu007 <kommukirankumar007@gmail.com>
2024-06-25 11:32:14 +0530

    added all

--- src/main/java/com/example/demo/controllers/OperationsControllers.java
+++ src/main/java/com/example/demo/controllers/OperationsControllers.java
@@ -76,19 +76,6 @@ public class OperationsControllers {
 	
 	
 	
-	
-	
-	
-	
-//	 @Operation(summary = "Welcome page", description = "Displays the welcome page")
-//	    @GetMapping("/welcome")
-//	    public String getWelcome() {
-//
-//	        return "/views/pages/welcome";
-//	    }
-
-	 
-	 
 	 
 	@Operation(summary = "Handle login", description = "Processes login and sets JWT token in cookie")
 	@PostMapping("/login")
@@ -117,44 +104,6 @@ public class OperationsControllers {
 	}
 	 
 	 
-
-//	    @Operation(summary = "Handle login", description = "Processes login and sets JWT token in cookie")
-//	    @PostMapping("/login")
-//	    public String login(@RequestParam String username, @RequestParam String password, Model model, HttpServletResponse response) {
-//	        logger.info("Login attempt for user: {}", username);
-//	        try {
-//	            Authentication authentication = authenticationManager
-//	                    .authenticate(new UsernamePasswordAuthenticationToken(username, password));
-//	            SecurityContextHolder.getContext().setAuthentication(authentication);
-//	            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-//	            String token = jwtTokenProvider.generateToken(userDetails);
-//	            String role = jwtTokenProvider.extractRoleAsString(token);
-//
-//	            Cookie cookie = new Cookie("token", token);
-//	            cookie.setHttpOnly(true);
-//	            cookie.setPath("/");
-//	            response.addCookie(cookie);
-//	            model.addAttribute("token", token);
-//	            model.addAttribute("username", userDetails.getUsername());
-//	            model.addAttribute("roles", role);
-//
-//	            logger.info("User {} authenticated with role {}", username, role);
-//
-//	            if (role.contains("ROLE_ADMIN")) {
-//	                return "redirect:/admin/home";
-//	            } else if (role.contains("ROLE_HR")) {
-//	                return "redirect:/hr/home";
-//	            } else if (role.contains("ROLE_USER")) {
-//	                return "redirect:/user/home";
-//	            }
-//
-//	        } catch (AuthenticationException e) {
-//	            logger.error("Authentication failed for user: {}", username, e);
-//	            model.addAttribute("error", "Invalid username or password");
-//	            return "/views/pages/welcome";
-//	        }
-//	        return "/views/pages/welcome";
-//	    }
 	
 
 	@GetMapping("/listemployees")
